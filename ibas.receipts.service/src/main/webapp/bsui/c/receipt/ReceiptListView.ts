a1fea0454fb29e0b797790b71f67a2378259e968
NiurenZhu <niuren.zhu@icloud.com>
2017-11-16 20:23:15 +0800

    去除空行

--- ibas.receipts.service/src/main/webapp/bsui/c/receipt/ReceiptListView.ts
+++ ibas.receipts.service/src/main/webapp/bsui/c/receipt/ReceiptListView.ts
@@ -232,7 +232,6 @@ export class ReceiptListView extends ibas.BOListView implements IReceiptListView
     /** 记录上次查询条件，表格滚动时自动触发 */
     query(criteria: ibas.ICriteria): void {
         super.query(criteria);
-
         // 清除历史数据
         if (this.isDisplayed) {
             this.table.setBusy(true);
